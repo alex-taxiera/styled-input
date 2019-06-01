d1fa5c5db1a2536430ab59d42c43761b28d4203d
Alex Taxiera <alex.taxiera@gmail.com>
2019-05-31 21:04:56 -0400

    update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import StyledInput, { useForm } from 'styled-input'
+import { StyledInput, useForm } from 'styled-input'
 
 import './App.css'
 
@@ -8,21 +8,27 @@ const App = () => {
   const [success, setSuccess] = useState(false)
 
   const {
-    values,
+    fields,
     hasErrors,
     isComplete,
-    handleChange,
-    handleSubmit
-  } = useForm({
-    name: {
+    handleSubmit,
+    handleChange
+  } = useForm([
+    {
+      name: 'name',
+      label: 'Your Name',
+      type: 'text',
       value: '',
       isRequired: true
     },
-    message: {
+    {
+      name: 'message',
+      label: 'Your Message',
+      type: 'textarea',
       value: '',
       isRequired: true
     }
-  }, () => {
+  ], () => {
     if (canSubmit) {
       setSuccess(true)
     } else {
@@ -30,6 +36,7 @@ const App = () => {
     }
   })
 
+  console.log('fields', fields)
   const canSubmit = !hasErrors && isComplete
 
   return (
@@ -42,35 +49,33 @@ const App = () => {
           <div className="success-message">
             Here's what you submitted:
             <ul>
-              {Object.entries(values).map(([key, { value }], i) => (
-                <li key={i}>
-                  <div>{key}</div>
-                  <div>{value}</div>
-                </li>
-              ))}
+              {
+                Object.entries(fields).map(([name, { value }], i) => (
+                  <li key={i}>
+                    <div>{name}</div>
+                    <div>{value}</div>
+                  </li>
+                ))
+              }
             </ul>
           </div>
         ) : (
           <form onSubmit={handleSubmit} disabled={!canSubmit}>
           <h2>My Cool Form</h2>
-            <StyledInput
-              name="name"
-              id="name"
-              label="Your Name"
-              type="text"
-              value={values.name.value}
-              isRequired
-              onChange={handleChange}
-            />
-            <StyledInput
-              name="message"
-              id="message"
-              label="Your Message"
-              type="textarea"
-              value={values.message.value}
-              isRequired
-              onChange={handleChange}
-            />
+            {
+              Object.values(fields).map((field, i) => (
+                <StyledInput
+                  key={i}
+                  name={field.name}
+                  id={field.name}
+                  label={field.label}
+                  type={field.type}
+                  value={field.value}
+                  isRequired={field.isRequired}
+                  onChange={handleChange}
+                />
+              ))
+            }
             <div className="error-message">{error && error.message}</div>
             <div className="submit-wrapper">
               <button type="submit" className={!canSubmit ? 'disabled' : ''}>
